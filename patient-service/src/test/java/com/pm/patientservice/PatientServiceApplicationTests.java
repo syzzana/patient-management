82e168061b815b6e6d18c448fb016f5fc34f1b60
syzzana <syzanabicaj@gmail.com>
2026-01-09 23:01:45 +0100

    36 producer and consumer test, fix kafka template bean by adding config

--- patient-service/src/test/java/com/pm/patientservice/PatientServiceApplicationTests.java
+++ patient-service/src/test/java/com/pm/patientservice/PatientServiceApplicationTests.java
@@ -2,9 +2,14 @@ package com.pm.patientservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class PatientServiceApplicationTests {
+
+    @MockitoBean
+    private KafkaTemplate<String, byte[]>  kafkaTemplate;
 
     @Test
     void contextLoads() {

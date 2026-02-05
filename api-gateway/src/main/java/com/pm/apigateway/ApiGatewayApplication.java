9109e1e747f338569a9e8269d8c34bec2ffee6e8
syzzana <syzana.bicaj@gmail.com>
2026-02-05 22:40:46 +0100

    43: setup api gateway debug add endpoint on ai-gateway

--- api-gateway/src/main/java/com/pm/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/pm/apigateway/ApiGatewayApplication.java
@@ -13,7 +13,6 @@ public class ApiGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
-        System.out.println("retest hello world");
     }
 
 }
